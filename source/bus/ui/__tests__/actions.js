7fdd592e073d120d8faa1e5200e3a8ba37f0229b
Yury Kornilov <kornilovyv81@gmail.com>
2019-03-02 01:47:36 +0300

    lesson-11 | homework

--- source/bus/ui/__tests__/actions.js
+++ source/bus/ui/__tests__/actions.js
@@ -1,40 +1,24 @@
-// Types
-import { types } from '../types';
-
 // Actions
 import { uiActions } from '../actions';
 
 describe('UI actions:', () => {
     test('startFetching', () => {
-        expect(uiActions.startFetching()).toEqual({
-            type: types.START_FETCHING,
-        });
+        expect(uiActions.startFetching()).toMatchSnapshot();
     });
 
     test('stopFetching', () => {
-        expect(uiActions.stopFetching()).toEqual({
-            type: types.STOP_FETCHING,
-        });
+        expect(uiActions.stopFetching()).toMatchSnapshot();
     });
 
     test('setOnlineState', () => {
-        expect(uiActions.setOnlineState()).toEqual({
-            type: types.SET_ONLINE_STATE,
-        });
+        expect(uiActions.setOnlineState()).toMatchSnapshot();
     });
 
     test('setOfflineState', () => {
-        expect(uiActions.setOfflineState()).toEqual({
-            type: types.SET_OFFLINE_STATE,
-        });
+        expect(uiActions.setOfflineState()).toMatchSnapshot();
     });
 
     test('emitError', () => {
-        expect(uiActions.emitError(new Error())).toEqual({
-            type:    types.EMIT_ERROR,
-            payload: new Error(),
-            error:   true,
-            meta:    null,
-        });
+        expect(uiActions.emitError(new Error())).toMatchSnapshot();
     });
 });
